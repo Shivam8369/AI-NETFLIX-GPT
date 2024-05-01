5de7ff6d1f2d93b419fc8dfb5d47533d4dbd7eec
shivam sahu <sahu.sd@somaiya.edu>
2024-05-01 10:30:18 +0530

    added css

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -125,11 +125,11 @@ const GptSearchBar = () => {
           <input
             ref={searchText}
             type="text"
-            className=" xl:text-base lg:text-base md:text-sm sm:text-sm text-xs rounded-l-full font-normal text-black border-black xl:pl-5 md:pl-4 sm:pl-4 pl-3 lg:pl-5 xl:py-3 lg:py-3 md:py-2.5 sm:py-2 py-1.5 xl:w-9/12 md:w-10/12 w-9/12 sm:w-10/12 lg:w-9/12"
+            className=" h-9 xl:text-base lg:text-base md:text-sm sm:text-sm text-xs rounded-l-full font-normal text-black border-black xl:pl-5 md:pl-4 sm:pl-4 pl-3 lg:pl-5 xl:py-3 lg:py-3 md:py-2.5 sm:py-2 py-1.5 xl:w-9/12 md:w-10/12 w-9/12 sm:w-10/12 lg:w-9/12"
             placeholder={lang[langKey].gptSearchPlaceholder}
           />
           <button
-            className="bg-red-700 xl:py-3  lg:py-3 md:py-4 sm:py-2 py-1.5 xl:px-8 sm:px-4 px-2 md:px-6 lg:px-8 font-semibold xl:text-base lg:text-base md:text-sm sm:text-sm text-xs  xl:w-3/12 md:w-2.5/12 sm:w-2/12 w-1.5/12 lg:w-3/12 rounded-r-full"
+            className="bg-red-700 xl:py-3  w-[24%] lg:py-3 md:py-4 sm:py-2 py-1.5 xl:px-8 sm:px-4 px-2 md:px-6 lg:px-8 font-semibold xl:text-base lg:text-base md:text-sm sm:text-sm text-xs  xl:w-3/12 md:w-2.5/12 sm:w-2/12 w-1.5/12 lg:w-3/12 rounded-r-full"
             onClick={handleGptSearchClick}
           >
             {loadingBtn ? (
